7f0729d734952033cbe03473ef92e63cd2cda518
asherzion <alok.a.boda@gmail.com>
2024-11-28 16:59:47 -0600

    testing e2e code pipeline

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,3 +6,7 @@ docker pull asherzion/simple-python-app
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 asherzion/simple-python-app
+
+# Started container 
+containerid=`docker ps | awk -F " " '{print $1}'`
+echo "started container $containerid"
